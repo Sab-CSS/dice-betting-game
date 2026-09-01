c948fc893d989e7ff18ea201ee814e33e8b66c8d
Sab-CSS <sabalmallick8@gmail.com>
2026-09-02 01:03:10 +0530

    just check

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const bankpass=prompt("enter your bank password")
 const bal=prompt("enter your balance")
+const play=document.getElementById("play")
+const inst=document.getElementById("instruction")
+const bank=document.getElementById("bank")
+const status=document.getElementById("status")
+const exit=document.getElementById("exit")
+
+exit.addEventListener("click",() =>{
+    window.close();
+});
