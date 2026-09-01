189954461fa27e30ab056f5f87e47fdc6146a498
Sab-CSS <sabalmallick8@gmail.com>
2026-09-02 01:20:03 +0530

    instruction

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ const inst=document.getElementById("instruction")
 const bank=document.getElementById("bank")
 const status=document.getElementById("status")
 const exit=document.getElementById("exit")
+const instructionmodel=document.getElementById("instruction-model")
+const closeInstruction=document.getElementById("close-instruction")
 
-exit.addEventListener("click",() =>{
+exit.addEventListener('click',() =>{
     window.close();
 });
+
+inst.addEventListener('click',() =>{
+    instructionmodel.style.display="block";
+})
+
+closeInstruction.addEventListener('click',() =>{
+    instructionmodel.style.display="none";
+})
