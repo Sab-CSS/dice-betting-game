ff3c13dace859e907d7f8d150a66659c0a315e0d
Sab-CSS <sabalmallick8@gmail.com>
2026-09-02 16:23:01 +0530

    minor

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const status=document.getElementById("status")
 const exit=document.getElementById("exit")
 const instructionmodel=document.getElementById("instruction-model")
 const closeInstruction=document.getElementById("close-instruction")
+const a=Math.floor(Math.random()*6)+1;
+const b=Math.floor(Math.random()*6)+1;
+const c=Math.floor(Math.random()*6)+1;
+let win=0,lose=0,debt=0;
 
 exit.addEventListener('click',() =>{
     window.close();
